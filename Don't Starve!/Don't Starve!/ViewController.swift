e958304b89450140ba0cc19485a98d6207fc89b3
Shermukhammad Usmonov <codingblyatt@gmail.com>
2024-01-23 20:35:00 -0500

    Created the starting solution for the project

---
+++ Don't Starve!/Don't Starve!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Don't Starve!
+//
+//  Created by Shermukhammad Usmonov on 2024-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
